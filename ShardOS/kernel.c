356ce7eaa890598bd8b1aa177ca00d94bb28750c
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-08-05 13:17:44 +0200

    studff

--- ShardOS/kernel.c
+++ ShardOS/kernel.c
@@ -1,13 +1,13 @@
 // kernel.c
 void kernel_main() {
     const char *str = "Hello World in ShardOS!";
-    char *vidptr = (char*)0xb8000; // Video memory starts at 0xb8000
+    char *vidptr = (char*)0xb8000;
     unsigned int i = 0;
     unsigned int j = 0;
 
     while (str[j] != '\0') {
         vidptr[i++] = str[j++];
-        vidptr[i++] = 0x07; // Attribute-byte: light grey on black screen
+        vidptr[i++] = 0x07;
     }
 
     return;
--- ShardOS/standalone.asm
+++ ShardOS/standalone.asm
@@ -2,7 +2,6 @@ BITS 16
 ORG 0x7C00
 
 start:
-    ; Initialize the stack
     cli
     xor ax, ax
     mov ds, ax
@@ -10,25 +9,20 @@ start:
     mov ss, ax
     mov sp, 0x7C00
 
-    ; Load the GDT
     lgdt [gdt_descriptor]
 
-    ; Enable A20 line
     in al, 0x92
     or al, 00000010b
     out 0x92, al
 
-    ; Switch to protected mode
     mov eax, cr0
     or eax, 1
     mov cr0, eax
 
-    ; Far jump to flush the pipeline and switch to 32-bit mode
     jmp 0x08:protected_mode
 
 BITS 32
 protected_mode:
-    ; Set up segment registers
     mov ax, 0x10
     mov ds, ax
     mov es, ax
@@ -37,7 +31,6 @@ protected_mode:
     mov ss, ax
     mov esp, 0x9C00
 
-    ; Call the kernel main function
     call kernel_main
 
 halt:
@@ -45,7 +38,6 @@ halt:
     jmp halt
 
 kernel_main:
-    ; Set video memory pointer
     mov esi, msg
     mov edi, 0xb8000
 
@@ -63,7 +55,6 @@ done:
 
 msg db 'Hello, ShardOS!', 0
 
-; GDT (Global Descriptor Table)
 gdt_start:
     dq 0x0000000000000000  ; Null descriptor
     dq 0x00CF9A000000FFFF  ; Code segment descriptor
