e230e369b62cc341189146194e40eb58a6f283e3
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-08-06 11:14:59 +0200

    Update boot.asm

--- ShardOS/boot/boot.asm
+++ ShardOS/boot/boot.asm
@@ -26,13 +26,18 @@ start:
     int 0x13
     jc disk_error
 
-    ; Switch to 32-bit protected mode
+    ; Jump to protected mode
     cli
     lgdt [gdt_descriptor]
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
     jmp CODE_SEG:init_pm
+
+disk_error:
+    mov si, disk_error_msg
+    call print_string
+    hlt
 
 BITS 32
 init_pm:
@@ -90,3 +95,5 @@ disk_error_msg db "Disk read error", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
+
+extern kernel_main
