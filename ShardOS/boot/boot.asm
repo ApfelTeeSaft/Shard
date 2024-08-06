66970c556a5ffb3443fb139454d593ec5f7df204
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-08-06 09:37:30 +0200

    Update boot.asm

--- ShardOS/boot/boot.asm
+++ ShardOS/boot/boot.asm
@@ -1,44 +1,56 @@
+; boot.asm
 BITS 16
 ORG 0x7C00
 
 start:
-    cli
+    ; Set up the stack
     xor ax, ax
     mov ds, ax
     mov es, ax
+    mov fs, ax
+    mov gs, ax
     mov ss, ax
-    mov sp, 0x7c00
+    mov sp, 0x7C00
 
     ; Load the second stage bootloader
-    mov si, msg
+    mov si, load_msg
     call print_string
 
-    ; Load the second stage bootloader
-    mov bx, 0x1000
-    mov dh, 1
-    mov dl, 0
-    mov ch, 0
-    mov cl, 2
+    ; Load the bootloader (2 sectors, starting at LBA 1)
+    mov bx, 0x0000   ; Segment to load at
+    mov dh, 0x02     ; Number of sectors
+    mov dl, 0x80     ; Drive number
+    mov cx, 0x0002   ; Starting LBA (logical block address)
+    call disk_load
 
-    mov ah, 0x02
-    mov al, 1
-    int 0x13
+    ; Jump to the bootloader
+    jmp 0x0000:0x0200
 
-    ; Jump to second stage bootloader
-    jmp 0x1000:0
+load_msg db "Loading second stage bootloader...", 0
 
 print_string:
-    mov ah, 0x0e
-.loop:
+    mov ah, 0x0E
+.repeat:
     lodsb
     cmp al, 0
     je .done
     int 0x10
-    jmp .loop
+    jmp .repeat
 .done:
     ret
 
-msg db "Loading second stage bootloader...", 0
+disk_load:
+    pusha
+    mov ah, 0x02   ; BIOS read sectors
+    int 0x13
+    jc .error
+    popa
+    ret
+.error:
+    mov si, error_msg
+    call print_string
+    hlt
+error_msg db "Disk read error", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
