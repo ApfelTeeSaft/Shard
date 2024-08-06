83e4fb8d7ef5bd9aa42e4dcb0aa41e51613eedad
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-08-06 11:12:28 +0200

    as

--- ShardOS/boot/boot.asm
+++ ShardOS/boot/boot.asm
@@ -11,11 +11,11 @@ start:
     mov ss, ax
     mov sp, 0x7C00
 
-    ; Load the second stage bootloader
+    ; Print loading message
     mov si, load_msg
     call print_string
 
-    ; Load the bootloader (2 sectors, starting at LBA 1)
+    ; Load the kernel (2 sectors, starting at LBA 1)
     mov bx, 0x0000   ; Segment to load at
     mov ah, 0x02     ; BIOS read sectors
     mov al, 0x02     ; Number of sectors to read
@@ -26,11 +26,36 @@ start:
     int 0x13
     jc disk_error
 
-    ; Jump to the bootloader
-    jmp 0x0000:0x0200
+    ; Switch to 32-bit protected mode
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:init_pm
 
-load_msg db "Loading second stage bootloader...", 0
-disk_error db "Disk read error", 0
+BITS 32
+init_pm:
+    ; Set up segment registers
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x9C00
+
+    ; Print bootloader message
+    mov si, bootloader_msg
+    call print_string
+
+    ; Call kernel main function
+    call kernel_main
+
+    ; Hang if the kernel returns
+halt:
+    hlt
+    jmp halt
 
 print_string:
     mov ah, 0x0E
@@ -42,6 +67,26 @@ print_string:
     jmp .repeat
 .done:
     ret
+
+gdt_start:
+    ; Null descriptor
+    dw 0, 0, 0, 0
+    ; Code segment descriptor
+    dw 0xFFFF, 0x0000, 0x9A00, 0x00CF
+    ; Data segment descriptor
+    dw 0xFFFF, 0x0000, 0x9200, 0x00CF
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_start + 8
+DATA_SEG equ gdt_start + 16
+
+load_msg db "Loading kernel...", 0
+bootloader_msg db "Kernel loaded, entering 32-bit mode...", 0
+disk_error_msg db "Disk read error", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
